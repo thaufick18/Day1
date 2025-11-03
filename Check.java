f3bef473b5a0ff794be2958100be9455ff1fa145
thaufick18 <311823205035@msajce-edu.in>
2025-11-03 15:06:05 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,8 @@
+
+class Check {
+    public static void main(String[] args) {
+        String a = "ABC";
+        String b = "Mumbai";
+        System.out.println(a + " Technologies " + b);
+    }
+}
