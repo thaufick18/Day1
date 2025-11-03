2cdd3d28a553459bb0d508e4468de87376108577
thaufick18 <311823205035@msajce-edu.in>
2025-11-03 15:12:43 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,8 @@
+class Add {
+    public static void main(String[] args) {
+        int num1 = 18;
+        int num2 = 34;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
