a6fae8ee2ebb15cc5c17d6d6ed9a948f3a768cca
thaufick18 <311823205035@msajce-edu.in>
2025-11-21 12:27:54 +0530

    Create Alphabet.java

---
+++ HW8/Alphabet.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Alphabet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            System.out.println("Alphabet");
+        else if (ch >= '0' && ch <= '9')
+            System.out.println("Digit");
+        else
+            System.out.println("Special Character");
+    }
+}
